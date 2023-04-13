9966a404d5b7b11433ffef6fc94d551dd15fe81c
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-04-13 17:44:28 -0400

    added cleanup, still verified working

--- build_docker.sh
+++ build_docker.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 # Build
-set -e
+set +e
+
+echo "[build_docker - $(date)] Started"
+
+export DOCKER_REPO_NAME="docker-cron-example"
+export CONTAINER_RUNTIME_NAME="$DOCKER_REPO_NAME-runtime"
 
 ## shellcheck ./*.sh
 # pycodestyle ./*.py
 # hadolint Dockerfile
 
-# set +e
-# docker image purge cron-example
-# set -e
+# stop and kill any related containers
+docker stop "$CONTAINER_RUNTIME_NAME" && docker rm $_  &> /dev/null
+docker container prune --force &> /dev/null
 
-docker build -t cron-example .
+# remove old unused images
 
-docker run cron-example
+docker rmi $(docker images "$DOCKER_REPO_NAME" -a -q)  &> /dev/null
+docker rmi $(docker images -f "dangling=true" -q) &> /dev/null
+
+
+# build it
+set -e
+docker build -t "$DOCKER_REPO_NAME" .
+
+docker run "$DOCKER_REPO_NAME"
+
+set +e
+docker container prune --force &> /dev/null
+docker images clean --quiet &> /dev/null
+docker builder prune --all --force &> /dev/null
+
+echo "[build_docker - $(date)] Finished successfully"
